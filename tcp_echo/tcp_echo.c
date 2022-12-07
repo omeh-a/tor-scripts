c30731c6b5cfee16ed440441c82157a11e5af06e
Matt Rossouw <39023043+omeh-a@users.noreply.github.com>
2022-12-07 14:14:54 +1100

    TCP/UDP initial

---
+++ tcp_echo/tcp_echo.c
@@ -0,0 +1,9 @@
+// Simple TCP echo - receives packets, and returns them.
+// Matt Rossouw
+// 07/12/2022
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string.h>
+
