78e970e71c32a4334a81de33e34315ceeb8710da
Matt Rossouw <39023043+omeh-a@users.noreply.github.com>
2023-01-26 00:04:58 +1100

    More cleanup, stuff for ivan's EOS/hotos submission

---
+++ driver-framework/ununmarshall.py
@@ -0,0 +1,18 @@
+import sys
+
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Specify file")
+        exit(0)
+
+    with open(sys.argv[1], "r") as r, open(f"{sys.argv[1]}-clean", "w") as w:
+        w.write("Requested_throughput,Achieved_throughput_sent,Achieved_throughput_received,Sent_size_received,Min,Avg,Max,Std-dev,Median")
+        for line in r:
+            if "[unmarshall]" in line or "started!" in line:
+                continue
+            w.write(line)
+
+if __name__ == "__main__":
+    main()
